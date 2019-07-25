d0cb61519534812a26fa4672b1e675d71b6fe7a0
wangxiaodong09 <wangxiaodong09@meituan.com>
2019-07-25 12:17:50 +0800

    init

---
+++ algorithm/tecent/src/main/java/MainTest.java
@@ -0,0 +1,13 @@
+/**
+ * @description:
+ * @author: wangxiaodong09
+ * @date: 2019-07-25
+ * @time: 12:11
+ * Copyright (C) 2015 MTDP ——>Copyright (C) 2019 MTDP
+ * All rights reserved
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
